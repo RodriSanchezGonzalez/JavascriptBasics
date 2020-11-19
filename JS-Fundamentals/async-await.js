213078fe21e52b7f2c953ebfe317ddd9e020f7b5
RodriSanchezGonzalez <sanchez.gonzalez.rodri@gmail.com>
2020-11-19 17:12:32 +0100

    #AsyncAwait Concat Promises with await

--- JS-Fundamentals/async-await.js
+++ JS-Fundamentals/async-await.js
@@ -13,3 +13,46 @@ const getNameEquals = () =>{
 getName().then(name => console.log(name))
          .catch( err => console.log(`ERROR en Async | ${err}` ));
 getNameEquals().then(name => console.log(name));
+
+/*Employees exercise */
+let employees =[{id: 1, name: 'Rodrigo'},
+                {id: 2, name: 'Pedro'},
+                {id: 3, name: 'Juan'}];
+
+let wages= [{ id: 1, amount: 1000},
+            { id: 2, amount: 1500}
+        ];
+
+
+const getEmployee = async (id) => {
+        let employeeDB = employees.find(employee => employee.id === id);
+        if(!employeeDB){
+            //Reject
+            throw new Error(`User with id: ${id}, not exists in database`);
+        }
+        else{
+            //Resolve
+            return(employeeDB);
+        };
+}
+
+const getWage = async (employee) =>{
+        let wageDB = wages.find(wage => wage.id === employee.id);
+        if(!wageDB){
+            //Reject
+            throw new Error(`Wage with id: ${employee.id}, not exists in database`);
+        }
+        else {
+             //Resolve
+            return({name: employee.name, wage: wageDB.amount});
+        };
+}
+
+const getInformation = async (id) => {
+    let employee = await getEmployee(id);
+    let response = await getWage(employee);
+    return 'Get information: ' + JSON.stringify(response);
+}
+
+getInformation(1).then(response => console.log(response))
+                 .catch(err => console.log('ERROR Get Information |' + err));
